690d84d5d76fcc741f449f20cb741fe5fba33e90
Uriel Olascoaga <uriel1396@gmail.com>
2024-02-22 10:12:21 -0600

    First commit with simple stacks, texts and buttons in layout

---
+++ SwiftUIBasics/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  SwiftUIBasics
+//
+//  Created by Uriel Olascoaga on 22/02/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 32) {
+            Text("SwiftUI Course")
+                .font(.largeTitle)
+                .foregroundColor(.blue)
+                .underline()
+            Text("Welcome to the course")
+                .font(.subheadline)
+                .foregroundColor(.green)
+                .bold()
+            Spacer()
+            HStack(alignment: .center, spacing: 8) {
+                Button(action: {}, label: {
+                    Label("ALERT", systemImage: "exclamationmark.triangle.fill")
+                })
+                .padding(.all)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(8)
+            }
+        }.padding(.all)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Uriel Olascoaga on 22/02/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
