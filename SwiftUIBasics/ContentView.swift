17f33852b1705a34338237f11ebd0afe3d0297cf
Uriel Olascoaga <uriel1396@gmail.com>
2024-03-11 10:50:49 -0600

    Added NavigationView to show dedicated counter view

--- SwiftUIBasics/ContentView.swift
+++ SwiftUIBasics/ContentView.swift
@@ -38,102 +38,117 @@ struct ContentView: View {
     }
     
     var body: some View {
-        VStack(alignment: .center, spacing: 32) {
-            Text("Welcome to the course \(name)")
-                .font(.subheadline)
-                .foregroundColor(.green)
-                .bold()
-            
-            Text("\(randomNumber.number)")
-                .font(.title)
-                .bold()
-            
-            HStack {
-                Button(action: {
-                    randomNumber.startTimer()
-                }) {
-                    Label("START", systemImage: "play.fill")
-                }
-                .padding(.all)
-                .foregroundColor(.white)
-                .background(Color.green)
-                .cornerRadius(8)
+        NavigationStack {
+            VStack(alignment: .center, spacing: 32) {
+                Text("Welcome to the course \(name)")
+                    .font(.subheadline)
+                    .foregroundColor(.green)
+                    .bold()
                 
-                Button(action: {
-                    randomNumber.stopTimer()
-                }) {
-                    Label("STOP", systemImage: "stop.fill")
+                Text("\(randomNumber.number)")
+                    .font(.title)
+                    .bold()
+                
+                HStack {
+                    Button(action: {
+                        randomNumber.startTimer()
+                    }) {
+                        Label("START", systemImage: "play.fill")
+                    }
+                    .padding(.all)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(8)
+                    
+                    Button(action: {
+                        randomNumber.stopTimer()
+                    }) {
+                        Label("STOP", systemImage: "stop.fill")
+                    }
+                    .padding(.all)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(8)
+                    SecondCounterView()
                 }
-                .padding(.all)
-                .foregroundColor(.white)
-                .background(Color.red)
-                .cornerRadius(8)
-                SecondCounterView()
-            }
-            
-            Spacer()
-            List {
-                Toggle(isOn: $isFavoriteEnabled) {
-                    Text("Favoritos")
-                }
-                ForEach(songs.filter { $0.isFavorite == isFavoriteEnabled }) { song in
-                    HStack {
-                        Image(song.albumImage)
-                            .resizable()
-                            .frame(width: 64, height: 64)
-                        VStack(alignment: .leading) {
-                            Text(song.name)
-                                .font(.title3)
-                                .bold()
-                            Text(song.artist)
-                                .font(.subheadline)
+                
+                Spacer()
+                List {
+                    Toggle(isOn: $isFavoriteEnabled) {
+                        Text("Favoritos")
+                    }
+                    ForEach(songs.filter { $0.isFavorite == isFavoriteEnabled }) { song in
+                        HStack {
+                            Image(song.albumImage)
+                                .resizable()
+                                .frame(width: 64, height: 64)
+                            VStack(alignment: .leading) {
+                                Text(song.name)
+                                    .font(.title3)
+                                    .bold()
+                                Text(song.artist)
+                                    .font(.subheadline)
+                            }
                         }
                     }
                 }
-            }
-            Spacer()
-            HStack(alignment: .center, spacing: 8) {
-                Button(action: {
-                    isAlertVisible.toggle()
-                }) {
-                    Label("ALERT", systemImage: "exclamationmark.triangle.fill")
+                Spacer()
+                NavigationLink(destination: CounterView()) {
+                    HStack {
+                        Image(systemName: "arrowshape.right.fill")
+                            .foregroundStyle(Color.white)
+                        Text("Go to counter view")
+                            .foregroundStyle(Color.white)
+                            .font(.headline)
+                    }
+                    .padding(.all)
+                    .background(Color.red)
                 }
-                .padding(.all)
-                .foregroundColor(.white)
-                .background(Color.blue)
-                .cornerRadius(8)
-                .alert("ALERT", isPresented: $isAlertVisible) {
-                    TextField("Name", text: $name)
-                    Button("Confirm") { }
-                } message: {
-                    Text("I prefer to give you a personalized greeting")
+                .navigationTitle(Text("Main view"))
+                .navigationBarTitleDisplayMode(.inline)
+                HStack(alignment: .center, spacing: 8) {
+                    Button(action: {
+                        isAlertVisible.toggle()
+                    }) {
+                        Label("ALERT", systemImage: "exclamationmark.triangle.fill")
+                    }
+                    .padding(.all)
+                    .foregroundColor(.white)
+                    .background(Color.blue)
+                    .cornerRadius(8)
+                    .alert("ALERT", isPresented: $isAlertVisible) {
+                        TextField("Name", text: $name)
+                        Button("Confirm") { }
+                    } message: {
+                        Text("I prefer to give you a personalized greeting")
+                    }
+                    
+                    Button(action: {
+                        addSongs()
+                    }) {
+                        Label("ADD SONGS", systemImage: "music.note")
+                    }
+                    .padding(.all)
+                    .foregroundColor(.white)
+                    .background(Color.red)
+                    .cornerRadius(8)
+    //                .confirmationDialog("MENU", isPresented: $isConfirmationDialogVisible) {
+    //                    Button("Galery", action: {
+    //                        print("Galery selected")
+    //                    })
+    //                    Button("Camera", action: {
+    //                        print("Camera selected")
+    //                    })
+    //                    Button(role: .cancel, action: {}) {
+    //                        Text("Cancel")
+    //                    }
+    //                } message: {
+    //                    Text("Select an option")
+    //                }
+                    CounterView()
                 }
-                
-                Button(action: {
-                    addSongs()
-                }) {
-                    Label("ADD SONGS", systemImage: "music.note")
-                }
-                .padding(.all)
-                .foregroundColor(.white)
-                .background(Color.red)
-                .cornerRadius(8)
-//                .confirmationDialog("MENU", isPresented: $isConfirmationDialogVisible) {
-//                    Button("Galery", action: {
-//                        print("Galery selected")
-//                    })
-//                    Button("Camera", action: {
-//                        print("Camera selected")
-//                    })
-//                    Button(role: .cancel, action: {}) {
-//                        Text("Cancel")
-//                    }
-//                } message: {
-//                    Text("Select an option")
-//                }
-                CounterView()
-            }
-        }.padding(.all)
+            }.padding(.all)
+        }
     }
 }
 
