bf4019cadeb806b439ff3452781cd2f07607d4ad
Uriel Olascoaga <uriel1396@gmail.com>
2024-04-09 09:55:24 -0600

    Added PhotosUI to add PhotosPicker

--- SwiftUIBasics/Modules/CoreDataImages/PlaceFormView.swift
+++ SwiftUIBasics/Modules/CoreDataImages/PlaceFormView.swift
@@ -6,10 +6,30 @@
 //
 
 import SwiftUI
+import PhotosUI
 
 struct PlaceFormView: View {
+    @State private var avatarItem: PhotosPickerItem?
+    @State private var avatarImage: Image?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
+
+            avatarImage?
+                .resizable()
+                .scaledToFit()
+                .frame(width: 300, height: 300)
+        }
+        .onChange(of: avatarItem) {
+            Task {
+                if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
+                    avatarImage = loaded
+                } else {
+                    print("Failed")
+                }
+            }
+        }
     }
 }
 
