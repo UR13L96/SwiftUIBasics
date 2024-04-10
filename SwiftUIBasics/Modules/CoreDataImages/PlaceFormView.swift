080f33e966787b3be0616994793f9fb637e5b83e
Uriel Olascoaga <uriel1396@gmail.com>
2024-04-10 10:49:51 -0600

    Selected image with confirm dialog and save into core data

--- SwiftUIBasics/Modules/CoreDataImages/PlaceFormView.swift
+++ SwiftUIBasics/Modules/CoreDataImages/PlaceFormView.swift
@@ -9,24 +9,65 @@ import SwiftUI
 import PhotosUI
 
 struct PlaceFormView: View {
-    @State private var avatarItem: PhotosPickerItem?
-    @State private var avatarImage: Image?
+    @Environment(\.managedObjectContext) private var viewContext
+    @State private var photosPicker: PhotosPickerItem?
+    @State private var selectedImage: Image?
+    @State private var isSourceMenuVisible = false
+    @State private var isPhotosPickerVisible = false
+    @State private var imageData: Data?
     
     var body: some View {
         VStack {
-            PhotosPicker("Select avatar", selection: $avatarItem, matching: .images)
-
-            avatarImage?
+            selectedImage?
                 .resizable()
-                .scaledToFit()
-                .frame(width: 300, height: 300)
+                .frame(width: 250, height: 250)
+                .clipShape(.rect(cornerRadius: 16))
+            
+            Button {
+                isSourceMenuVisible = true
+            } label: {
+                Text("Load image")
+            }
+            .confirmationDialog("Menu", isPresented: $isSourceMenuVisible) {
+                Button("Gallery", action: {
+                    isPhotosPickerVisible = true
+                })
+                Button("Camera", action: {
+                    print("Camera selected")
+                })
+                Button(role: .cancel, action: {}) {
+                    Text("Cancel")
+                }
+            } message: {
+                Text("Select an option")
+            }
+            .photosPicker(isPresented: $isPhotosPickerVisible, selection: $photosPicker)
+
+            Button {
+                let place = Place(context: viewContext)
+                place.name = "Toluca"
+                if let data = imageData {
+                    place.image = data
+                }
+                
+                do {
+                    try viewContext.save()
+                } catch let error {
+                    print(error.localizedDescription)
+                }
+            } label: {
+                Text("Save image")
+            }
+
         }
-        .onChange(of: avatarItem) {
+        .onChange(of: photosPicker) {
             Task {
-                if let loaded = try? await avatarItem?.loadTransferable(type: Image.self) {
-                    avatarImage = loaded
-                } else {
-                    print("Failed")
+                if let loaded = try? await photosPicker?.loadTransferable(type: Image.self) {
+                    selectedImage = loaded
+                }
+                
+                if let loaded = try? await photosPicker?.loadTransferable(type: Data.self) {
+                    imageData = loaded
                 }
             }
         }
