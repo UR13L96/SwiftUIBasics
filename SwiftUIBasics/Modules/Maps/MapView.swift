1777a8f286678ebcc059e267e6ae2a748ec1bbec
Uriel Olascoaga <uriel1396@gmail.com>
2024-04-22 10:50:49 -0600

    Added map and pin with SwiftUI

--- SwiftUIBasics/Modules/Maps/MapView.swift
+++ SwiftUIBasics/Modules/Maps/MapView.swift
@@ -6,10 +6,19 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct MapView: View {
+    let hollywood = CLLocationCoordinate2D(
+        latitude: 34.0944781,
+        longitude: -118.3559678
+    )
+    
     var body: some View {
-        Text("MapView")
+        Map(initialPosition: .region(MKCoordinateRegion(center: hollywood, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))) {
+            Marker("Holywood", coordinate: hollywood)
+                .tint(.purple)
+        }
     }
 }
 
