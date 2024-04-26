81c8bbee4311ef4e68b4a66e626ae346ffea6e15
Uriel Olascoaga <uriel1396@gmail.com>
2024-04-26 10:09:20 -0600

    Added SwiftUI with user location

--- SwiftUIBasics/Modules/Maps/LocationView.swift
+++ SwiftUIBasics/Modules/Maps/LocationView.swift
@@ -6,10 +6,27 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationView: View {
+    let hollywood = CLLocationCoordinate2D(
+        latitude: 34.0944781,
+        longitude: -118.3559678
+    )
+    
     var body: some View {
-        Text("LocationView")
+        Map(initialPosition: .region(MKCoordinateRegion(center: hollywood, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))) {
+            Marker("Holywood", coordinate: hollywood)
+                .tint(.purple)
+            
+            UserAnnotation()
+        }
+        .mapStyle(.standard(elevation: .realistic))
+        .mapControls {
+            MapUserLocationButton()
+            MapCompass()
+            MapScaleView()
+        }
     }
 }
 
