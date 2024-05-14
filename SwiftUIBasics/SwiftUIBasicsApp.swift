4d8ac61d343ee2e30a05834308c0e72af76cc756
Uriel Olascoaga <uriel1396@gmail.com>
2024-05-14 10:11:35 -0600

    Added migration configuration for Realm

--- SwiftUIBasics/SwiftUIBasicsApp.swift
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -6,15 +6,30 @@
 //
 
 import SwiftUI
+import RealmSwift
 
 @main
-struct SwiftUIBasicsApp: App {
+struct SwiftUIBasicsApp: SwiftUI.App {
     let persistenceController = PersistenceController.shared
+    
+    init() {
+        setupRealm()
+    }
     
     var body: some Scene {
         WindowGroup {
             MenuView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
+    }
+    
+    private func setupRealm() {
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: { migration, oldSchemaVersion in
+                if (oldSchemaVersion < 1) {  }
+            }
+        )
+        Realm.Configuration.defaultConfiguration = config
     }
 }
