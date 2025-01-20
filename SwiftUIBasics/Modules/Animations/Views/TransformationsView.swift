3329fe0596f41d9798aa38a6995f26caae0ad638
Uriel Olascoaga <uriel1396@gmail.com>
2025-01-20 11:22:58 -0600

    Added transformation example to change shape of button

--- SwiftUIBasics/Modules/Animations/Views/TransformationsView.swift
+++ SwiftUIBasics/Modules/Animations/Views/TransformationsView.swift
@@ -8,8 +8,24 @@
 import SwiftUI
 
 struct TransformationsView: View {
+    @State private var color = Color.blue
+    @State private var radius: CGFloat = 0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Button {
+                radius = radius == 0 ? 100 : 0
+                color = color == .blue ? .red : .blue
+            } label: {
+                Text("Animate")
+                    .foregroundStyle(Color.white)
+                    .frame(width: 200, height: 200)
+                    .background(color)
+                    .clipShape(RoundedRectangle(cornerRadius: radius))
+                    .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: color)
+            }
+
+        }
     }
 }
 
