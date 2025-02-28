b86c9368a9c8acaf90597ab053147e0ba0fe0011
Uriel Olascoaga <uriel1396@gmail.com>
2025-02-28 10:10:37 -0600

    Add translation for alert and a date

--- SwiftUIBasics/Modules/Translation/TranslationView.swift
+++ SwiftUIBasics/Modules/Translation/TranslationView.swift
@@ -8,9 +8,38 @@
 import SwiftUI
 
 struct TranslationView: View {
+    @State private var isShowingAlert = false
+    @State private var date: Date = Date()
+    
     var body: some View {
-        Text("title")
-            .font(.largeTitle)
+        VStack {
+            Text("title")
+                .font(.largeTitle)
+            
+            Button {
+                isShowingAlert.toggle()
+            } label: {
+                Text("translation.show.alert")
+            }
+            .alert("translation.alert.title", isPresented: $isShowingAlert) {
+                Button {
+                    
+                } label: {
+                    Text("translation.button.confirm")
+                }
+
+            } message: {
+                Text("translation.button.message")
+            }
+
+            Form {
+                DatePicker(selection: $date, displayedComponents: .date) {
+                    Text("translation.date.title")
+                }
+                
+                Text("\(date)")
+            }
+        }
     }
 }
 
