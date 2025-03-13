f6964d91fab557fdef541b0f3b83128743d0679e
Uriel Olascoaga <uriel1396@gmail.com>
2025-03-13 10:03:13 -0600

    Add images recognizer functionality

--- SwiftUIBasics/Modules/VisionImages/VisionImageDetailView.swift
+++ SwiftUIBasics/Modules/VisionImages/VisionImageDetailView.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import Vision
+import UIKit
 
 struct VisionImageDetailView: View {
     let visionModel: VisionModel
@@ -18,14 +20,42 @@ struct VisionImageDetailView: View {
                 .aspectRatio(contentMode: .fit)
             
             Button {
-                
+                classifyImage()
             } label: {
                 Text("Scan")
             }
             
             Text(result)
+                .font(.largeTitle)
+            
             Spacer()
         }.navigationTitle(visionModel.name)
+    }
+    
+    func classifyImage() {
+        DispatchQueue.main.async {
+            var requests = [VNRequest]()
+            let image = UIImage(named: visionModel.name)
+            
+            do {
+                let modelML = try VNCoreMLModel(for: MLModel(contentsOf: ImageClassifier.urlOfModelInThisBundle))
+                let classification = VNCoreMLRequest(model: modelML) { request, error in
+                    if let result = request.results as? [VNClassificationObservation] {
+                        self.result = result.first?.identifier ?? "No result"
+                    }
+                }
+                
+                requests = ([classification])
+                let requestHandler = VNImageRequestHandler(data: image!.pngData()!, options: [:])
+                do {
+                    try requestHandler.perform(requests)
+                } catch {
+                    print(error.localizedDescription)
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
     }
 }
 
